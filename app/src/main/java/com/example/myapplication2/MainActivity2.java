103468b77fc0bd1b95ecefc8640bd2f689ed0d5e
Aqyla Cantik <fithaqila@gmail.com>
2022-03-07 23:16:06 +0700

    activity 3

---
+++ app/src/main/java/com/example/myapplication2/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView txEmail, txPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        txEmail = findViewById(R.id.tvEmail);
+        txPassword = findViewById(R.id.tvPassword);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String email = bundle.getString("a");
+        String pwd = bundle.getString("b");
+
+        txEmail.setText(email);
+        txPassword.setText(pwd);
+    }
+}
